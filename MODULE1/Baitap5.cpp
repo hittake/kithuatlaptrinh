2887117ba1e4ab06f54fee142b5e06cd3ae7bd11
hittake <luongminhduc411@gmail.com>
2026-01-21 21:01:10 +0700

    Bai tap 5

--- MODULE1/Baitap5.cpp
+++ MODULE1/Baitap5.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <cmath>
 
+using namespace std;
+int main() {
+	float time, overtime, pay;
+	const float bonus = 1.5;
+	
+	cout << "Nhap tong so gio lam viec: "; cin >> time;
+	cout << "Nhap tong so gio lam them: "; cin >> overtime;
+	cout << "Nhap so tien tra moi gio: "; cin >> pay;
+	
+	cout << "So tien tra cho so gio lam viec thuong: " << time*pay << "\n";
+	cout << "So tien tra cho so gio lam them: " << overtime*(pay+pay*bonus) << "\n";
+	
+	return 0;
+}
