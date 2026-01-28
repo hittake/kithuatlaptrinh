1bd34f13acbbe4bc65b6930cc84d8769e458cc70
hittake <luongminhduc411@gmail.com>
2026-01-28 20:26:40 +0700

    Sua lai bai 2

--- MODULE1/Baitap02.cpp
+++ MODULE1/Baitap02.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
 #include <cmath>
 
+using namespace std;
+
 int main() {
-	float x,y;
-	int n;
-	std::cout << "Nhap 2 so thuc x, y: "; std::cin >> x >> y;
-	std::cout << "Nhap so n: "; std::cin >> n;
+    float x, y;
+    int n;
+
+    cout << "Nhap 2 so thuc x, y: "; 
+    cin >> x >> y;
+    cout << "Nhap so n: "; 
+    cin >> n;
+
+    float t = sqrt(pow(x, 5) + exp(log(abs(y)) + 1)) + (1 + sin(x)) / (cos(n * x) + sqrt(2 + abs(n)));
+
+
+    float s = abs(x - sqrt(17)) * (y + log10(abs(x) + 25) + y);
 
 	
-	float t = sqrt(pow(x, 5) + exp(log10(y)+1)) + (1+sin(x)) / (cos(n*x) + sqrt(2 + abs(n)));
-	float s = abs(x - sqrt(17)) * (y + log10(abs(x)+25)) + y;
-	std::cout << t << std::endl << s;
-	return 0;
-	
+    cout << "T = " << t << endl;
+    cout << "S = " << s << endl;
+
+    return 0;
 }
